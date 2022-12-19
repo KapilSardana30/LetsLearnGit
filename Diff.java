c246b4eeb0f1a074ea44503ae95bbbf639c59ece
kapil Sardana <Kapilsardana1999@gmail.com>
2022-12-19 22:30:53 +0530

    last commit

--- Diff.java
+++ Diff.java
@@ -2,7 +2,11 @@ public class Diff{
     public static void main(String [] args){
         int a = 5;
         int b = 6;
-    
+     
+        System.out.println("The sum is " + (a-b));
+
+
+        ///////////changed a bit in diff .java
         System.out.println("The sum is " + (a-b));
     }
 }
